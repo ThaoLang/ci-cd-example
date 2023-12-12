c8b6d9e659aa07106892274437ddbed6f5ef1b58
ThaoLang <90896633+ThaoLang@users.noreply.github.com>
2023-12-12 18:56:49 +0700

    fix: fix odd

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -8,8 +8,8 @@ import Home from "./page";
 it("Home: Works with Home Components (React State)", () => {
   render(<Home />);
   expect(screen.getByRole("heading")).toHaveTextContent("Click a button ....");
-  fireEvent.click(screen.getByRole("button", { name: "button-1" }));
+  fireEvent.click(screen.getByTestId("button-1"));
   expect(screen.getByRole("heading")).toHaveTextContent("Button 1");
-  fireEvent.click(screen.getByRole("button", { name: "Button 2" }));
+  fireEvent.click(screen.getByTestId("button-2"));
   expect(screen.getByRole("heading")).toHaveTextContent("button-2");
 });
